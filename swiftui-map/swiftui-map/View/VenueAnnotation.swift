c219f4a0886c31ff2e8ddf2ce7272a578f3b870e
Yurii Zadoianchuk <yurii@styleshoots.com>
2021-01-24 14:09:56 +0100

    Add animation to VenueAnnotation

--- swiftui-map/swiftui-map/View/VenueAnnotation.swift
+++ swiftui-map/swiftui-map/View/VenueAnnotation.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct VenueAnnotation: View {
     let vm: VenueAnnotationVM
+    @State var scale: CGFloat = 0.0
 
     var body: some View {
         Image(uiImage: vm.image.withRenderingMode(.alwaysTemplate))
@@ -22,6 +23,30 @@ struct VenueAnnotation: View {
                 Circle()
                     .stroke(Color.white, lineWidth: 1.5)
             ).shadow(color: .primary, radius: 3.0)
+            .scaleEffect(scale)
+            .onAppear(perform: self.onAppear)
+            .onDisappear {
+                self.scale = 0.0
+            }
+    }
+
+    func onAppear() {
+        DispatchQueue.main.asyncAfter(
+            deadline: .now() + .milliseconds(randomDelay * 100)
+            ) {
+                withAnimation(.interpolatingSpring(
+                                mass: 1.0,
+                                stiffness: 200.0,
+                                damping: 10,
+                                initialVelocity: 20
+                )) {
+                    self.scale = 1.0
+                }
+            }
+    }
+
+    var randomDelay: Int {
+        Int.random(in: 0..<3)
     }
 }
 
